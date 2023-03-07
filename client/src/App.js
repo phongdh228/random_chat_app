26947915592d6bf0bb79c694aeebe9751bdf2f35
Dang Hoang Phong <phongdh228@gmail.com>
2023-03-07 16:02:54 +0700

    create routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,51 @@
 import React from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+/*import all components */
+import Username from './components/Username'
+import Password from './components/Password'
+import PageNotFound from './components/PageNotFound'
+import Profile from './components/Profile'
+import Recovery from './components/Recovery'
+import Register from './components/Register'
+import Reset from './components/Reset'
+
+/*root router */
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Username></Username>
+  },  
+  {
+    path: '/register',
+    element: <Register></Register>
+  },
+  {
+    path: '/password',
+    element: <Password></Password>
+  },
+  {
+    path: '/pagenotfound',
+    element: <PageNotFound></PageNotFound>
+  },
+  {
+    path: '/profile',
+    element: <Profile></Profile>
+  },
+  {
+    path: '/recovery',
+    element: <Recovery></Recovery>
+  },
+  {
+    path: '/reset',
+    element: <Reset></Reset>
+  }
+])
 
 export default function App() {
   return (
-    <div>REact Login App</div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   )
 }
