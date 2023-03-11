3d6d7bccf8fe88ad52e8a6febdaefcc9d67eae7f
Dang Hoang Phong <phongdh228@gmail.com>
2023-03-11 15:26:04 +0700

    register UI

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -26,6 +26,15 @@ export async function resetPasswordValidation(values) {
 }
 /************************** */
 
+/*validate register form*/
+export async function registerValidation(values) {
+    const errors = usernameVerify({}, values);
+    passwordVerify(errors, values);
+    emailVerify(errors, values);
+
+    return errors;
+}
+
 /*validate password */
 function passwordVerify(errors ={} ,values) {
 
@@ -51,6 +60,21 @@ function usernameVerify(error = {}, values){
     }
     else if(values.username.includes(" ")){
         error.username = toast.error("Invalid username");
+    }
+
+    return error;
+}
+
+/*validate email address */
+function emailVerify(error = {}, values){
+    if(!values.email){
+        error.email = toast.error("Email Required");
+    }
+    else if(values.email.includes(" ")){
+        error.email = toast.error("Wrong email");
+    }
+    else if(!/[A-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+        error.email = toast.error("Invalid email");
     }
 
     return error;
