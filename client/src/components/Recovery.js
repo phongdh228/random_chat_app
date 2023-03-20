3eaea160ee2f913f601f482f7a01d6fe4600fcac
Dang Hoang Phong <phongdh228@gmail.com>
2023-03-20 16:37:48 +0700

    sync reset password

--- client/src/components/Recovery.js
+++ client/src/components/Recovery.js
@@ -1,25 +1,52 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import avatar from '../assets/avt.jpg';
 import {Toaster} from 'react-hot-toast';
-import {useFormik} from 'formik';
-import {passwordValidate} from '../helper/validate';
+import {useAuthStore} from '../store/store';
+import {generateOTP, verifyOTP} from '../helper/helper'
+import {userNavigate} from 'react-router-dom'
 
 import styles from '../styles/Username.module.css'
 
 export default function Recovery() {
 
-  const formik = useFormik({
-    initialValues: {
-      password:''
-    },
-    validate: passwordValidate,
-    validateOnBlur: false,
-    validateOnChange: false,
-    onSubmit: async values =>{
-      console.log(values)
+  const {username} = useAuthStore(state => state.auth)
+  const [OTP, setOTP] = useState();
+  const navigate = userNavigate()
+
+  useEffect(()=>{
+    generateOTP(username).then((OTP) =>{
+        if(OTP) return toast.success('OTP has been sent to your email');
+        return toast.error('Problems while generating OTP')
+    })
+  },[username])
+
+  async function onSubmit(e){
+    e.preventDefault();
+
+    try{
+      let {status} = await verifyOTP({username, code: OTP})
+      if(status === 201){
+        toast.success('Verify OTP successfully')
+        navigate('/reset')
+      }
+    }catch(e){
+      return toast.error('Wrong OTP')
     }
-  })
+  }
+
+  //Handle of resend OTP
+  function resendOTP() {
+    let sendPromise = generateOTP(username);
+
+    toast.promise(sendPromise,{
+      loading: 'Sending...',
+      success: <b>OTP has been sent to your email</b>,
+      error: <b>Could not sent it</b>
+    });
+
+    sendPromise.then(OTP => {
+      console.log(OTP);
+    })
+  }
 
   return (
     <div className='container mx-auto'>
@@ -36,7 +63,7 @@ export default function Recovery() {
               </span>
           </div>
 
-          <form className='pt-20' onSubmit={formik.handleSubmit}>
+          <form className='pt-20' onSubmit={onSubmit}>
 
             <div className='text-box flex flex-col items-center py-4'>
               
@@ -47,22 +74,23 @@ export default function Recovery() {
                 Enter 6 digit OTP sent to your email address
               </span>
               
-              <input className={styles.textbox} type="text" placeholder='OTP'/>
+              <input onChange={(e)=> setOTP(e.target.value)} className={styles.textbox} type="text" placeholder='OTP'/>
 
               </div>
 
               <button className={styles.btn} type="submit">Sign In</button>
             </div>
 
+          </form>
+
             <div className='text-box py-4 text-center'>
               <span className='text-gray-500'>
                 Can't get OTP?   
-                  <button className='text-red-500'>
+                  <button onClick={resendOTP} className='text-red-500'>
                     Resend
                   </button>
               </span>
             </div>
-          </form>
 
         </div>
       </div>
