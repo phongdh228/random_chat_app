f5e969ebd7588d80a429db84de4f6e20c29c8ec5
Dang Hoang Phong <phongdh228@gmail.com>
2023-04-03 15:08:16 +0700

    connect to postgresql

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,14 +1,40 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-import { MongoMemoryServer } from "mongodb-memory-server";
+// import { MongoMemoryServer } from "mongodb-memory-server";
+
+// async function connect(){
+//     const mongodb = await MongoMemoryServer.create();
+//     const getUri = mongodb.getUri();
+
+//     const db = await mongoose.connect(getUri);
+//     console.log("Database connected");
+//     return db;
+// }
+
+// export default connect;
+
+import { Client } from 'pg';
+
+const dotenv = require('dotenv');
+dotenv.config()
 
 async function connect(){
-    const mongodb = await MongoMemoryServer.create();
-    const getUri = mongodb.getUri();
+    try{
+        const client = new Client({
+            user: process.env.PGUSER,
+            host: process.env.PGHOST,
+            database: process.env.PGDATABASE,
+            password: process.env.PGPASSWORD,
+            port: process.env.PGPORT
+        });
 
-    const db = await mongoose.connect(getUri);
-    console.log("Database connected");
-    return db;
+        await client.connect();
+        console.log('Database connection established');
+        return client;
+    }
+    catch(error){
+        console.log(error);
+    }
 }
 
 export default connect;
